54f21c040f8f901e9e6f781020e8c362b193b59b
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2016-03-07 00:55:14 -0500

    occ

--- src/occ_action.cc
+++ src/occ_action.cc
@@ -1,3 +1,4 @@
+
 #include <occ_action.h>
 #include <algorithm>
 #include <occ.h>
@@ -302,6 +303,8 @@ uint64_t OCCAction::stable_copy(uint64_t key, uint32_t table_id, void *record)
                         barrier();
                         if (after_read == ret)
                                 return ret;
+                        else if (READ_COMMITTED)
+                                continue;
                         else
                                 throw occ_validation_exception(READ_ERR);
                         return ret;
