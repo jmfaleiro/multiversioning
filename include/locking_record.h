bf5a1e3e5c52674bb8848a85ee63cf7ba98d565e
Jose Faleiro <faleiro.jose.manuel@gmail.com>
2016-09-09 10:55:59 -0700

    Add locking record header.

---
+++ include/locking_record.h
@@ -0,0 +1,9 @@
+#ifndef 	LOCKING_RECORD_H_
+#define 	LOCKING_RECORD_H_
+
+#include <mcs_rw.h>
+
+#define LOCKING_RECORD_SIZE(value_sz) (sizeof(mcs_rw::mcs_rw_lock) + value_sz)
+#define LOCKING_VALUE_PTR(record) &(((mcs_rw::mcs_rw_lock*)record)[1]);
+
+#endif 		// LOCKING_RECORD_H_
